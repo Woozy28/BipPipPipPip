d7e6a4a84957d778b788ce567b740b8491a55da6
Woozy28 <mazepa.t@icloud.com>
2023-07-05 15:27:28 +1000

    set BPM from list

--- app/src/main/java/com/example/bippippippip/MainActivity.kt
+++ app/src/main/java/com/example/bippippippip/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.bippippippip
 
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -8,7 +7,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.GridView
 import android.widget.SeekBar
-import android.widget.Spinner
+import androidx.core.view.get
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,18 +24,19 @@ class MainActivity : AppCompatActivity() {
         val bpmspin: SeekBar = findViewById(R.id.seekBar)
         val songinput: EditText = findViewById(R.id.input_song)
         val addnew: Button = findViewById(R.id.add_but)
+        //val deletesong: Button = findViewById(R.id.del_but)
 
-        //добавляем новый трек в список отображения и ключ для БПМ
-        addnew.setOnClickListener {
-            songmap.put(songinput.text.toString(),bpminput.text.toString().toInt())
-            viewsong.add(songinput.text.toString())
-
-        }
 
         //адаптер для списка
         val songlistadapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,viewsong)
         songview.adapter = songlistadapter
 
+        songview.setOnItemClickListener { parent, view, position, id ->
+            bpminput.setText(songmap.get(songlistadapter.getItem(position).toString()).toString())
+
+        }
+
+        //листенер сикбара
         bpmspin.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 bpminput.setText(progress.toString())
@@ -52,12 +53,14 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+        //добавление трека в список
         addnew.setOnClickListener {
-            songmap.put(songinput.text.toString(),bpminput.text.toString().toInt())
-            viewsong.add(songinput.text.toString())
+            songmap.put(songinput.text.toString(),bpminput.text.toString().toInt()) //добавили название_трека : БПМ
+            viewsong.add(songinput.text.toString()) // добавили название трека для списка фронта
             songlistadapter.notifyDataSetChanged()
 
         }
+
 
     }
 
